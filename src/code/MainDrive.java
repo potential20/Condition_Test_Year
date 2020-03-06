e6bebdefbe33ba0dd51fd3adf047443cef1274f3
potential20 <passion212@nate.com>
2020-03-06 10:48:16 +0900

    100으로 나눠본 겨우 대처
    -나눠떨어지면 재검사 필요
    -안나눠떨어지면 윤년

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -19,6 +19,15 @@ public class MainDrive {
 			}
 			else {
 //				100으로 나누고 다시 판단해야함.
+				
+				if (year % 100 == 0) {
+//					400으로 나눠봐야함.
+				}
+				else {
+//					4로만 나눠지니까 윤년이 맞다.
+					System.out.println("윤년이 맞음");
+					System.out.println("4로는 나눠지지만 100으로는 안됨.");
+				}
 			}
 	}
 
