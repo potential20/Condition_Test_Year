56be0f0fade29982474f50e15778959ce382b969
potential20 <passion212@nate.com>
2020-03-06 10:34:24 +0900

    년도 입력기능
    - 스캐너를 입력해서 int로 받았다.

---
+++ src/code/MainDrive.java
@@ -0,0 +1,18 @@
+package code;
+
+import java.util.Scanner;
+
+public class MainDrive {
+	
+//	새 한글은 잘 됩니다.
+	
+	public static void main(String[] args) {
+		
+			Scanner scan = new Scanner(System.in);
+			System.out.println("궁금한 년도 입력 : ");
+			int year = scan.nextInt();
+			
+			System.out.println(year);
+	}
+
+}
